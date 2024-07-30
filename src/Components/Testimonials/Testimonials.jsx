ea0f773379880d4ceb6b11848c510cdeec940270
Adhish-Kumar-Baranwal <adhish0725@gmail.com>
2024-07-31 00:28:02 +0530

    add: initial files

---
+++ src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,92 @@
+import React, { useRef } from 'react'
+import './Testimonials.css'
+import next_icon from '../../assets/next-icon.png'
+import back_icon from '../../assets/back-icon.png'
+import user_1 from '../../assets/user-1.png'
+import user_2 from '../../assets/user-2.png'
+import user_3 from '../../assets/user-3.png'
+import user_4 from '../../assets/user-4.png'
+
+const Testimonials = () => {
+
+    const slider  = useRef();
+    let translate_x = 0;
+    
+    const slideForward = () => {
+        if(translate_x > -50){
+            translate_x -= 25
+        }
+        slider.current.style.transform = `translateX(${translate_x}%)`
+    }
+
+    const slideBackward = () => {
+        if(translate_x < 0){
+            translate_x += 25
+        }
+        slider.current.style.transform = `translateX(${translate_x}%)`
+    }
+
+  return (
+    <div className='testimonials' id='testimonials'>
+        <img src={next_icon} alt="" className='next-btn' onClick={slideForward} />
+        <img src={back_icon} alt="" className='back-btn' onClick={slideBackward} />
+        <div className="slider">
+            <ul ref={slider}>
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_1} alt="" />
+                            <div>
+                                <h3>Christina Lane</h3>
+                                <span>Esusity, USA</span>
+                            </div>
+                        </div>
+                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state of the art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                    </div>
+                </li>
+
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_2} alt="" />
+                            <div>
+                                <h3>William Jackson</h3>
+                                <span>Esusity, USA</span>
+                            </div>
+                        </div>
+                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state of the art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                    </div>
+                </li>
+
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_3} alt="" />
+                            <div>
+                                <h3>Claire Rain</h3>
+                                <span>Esusity, USA</span>
+                            </div>
+                        </div>
+                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state of the art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                    </div>
+                </li>
+
+                <li>
+                    <div className="slide">
+                        <div className="user-info">
+                            <img src={user_4} alt="" />
+                            <div>
+                                <h3>Tom Elsion</h3>
+                                <span>Esusity, USA</span>
+                            </div>
+                        </div>
+                        <p>Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state of the art facilities, and commitment to academic excellence have truly exceeded my expectations.</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Testimonials
